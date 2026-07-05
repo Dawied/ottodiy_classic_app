8b9092769ccad3f29cd33cd76a21584cd5308fcf
David <asimov6@gmail.com>
2026-07-05 12:04:28 +0200

    Drive Buzzer from OttoW_BLE_v2.ino itself instead of from Otto lib

--- firmware/OttoW_BLE_v2/OttoW_BLE_v2.ino
+++ firmware/OttoW_BLE_v2/OttoW_BLE_v2.ino
@@ -22,6 +22,7 @@
 
 #ifdef ARDUINO_ARCH_ESP32
 #include <ESP32Servo.h>
+#include "driver/gpio.h"
 #else
 #include <Servo.h>
 #endif
@@ -34,8 +35,115 @@
 #define BLE_RX 12
 #define BUZZER 10
 
-#include <Otto.h>
-Otto Ottobot;
+// Self-contained sound note definitions (copied from Otto DIY Library)
+#define note_E5  659.26
+#define note_B5  987.77
+#define note_C6  1046.50
+#define note_E6  1318.51
+#define note_G6  1567.98
+#define note_A6  1760.00
+#define note_D7  2349.32
+
+void _tone(float noteFrequency, long noteDuration, int silentDuration) {
+  if (silentDuration == 0) { silentDuration = 1; }
+  tone(BUZZER, (unsigned int)noteFrequency, noteDuration);
+  delay(noteDuration);
+  noTone(BUZZER);
+  delay(silentDuration);
+}
+
+void bendTones(float initFrequency, float finalFrequency, float prop, long noteDuration, int silentDuration) {
+  if (silentDuration == 0) { silentDuration = 1; }
+  if (initFrequency < finalFrequency) {
+    for (float i = initFrequency; i < finalFrequency; i = i * prop) {
+      _tone(i, noteDuration, silentDuration);
+    }
+  } else {
+    for (float i = initFrequency; i > finalFrequency; i = i / prop) {
+      _tone(i, noteDuration, silentDuration);
+    }
+  }
+}
+
+void sing(int songName) {
+  switch (songName) {
+    case 1: // S_connection
+      _tone(note_E5, 50, 30);
+      _tone(note_E6, 55, 25);
+      _tone(note_A6, 60, 10);
+      break;
+    case 2: // S_disconnection
+      _tone(note_E5, 50, 30);
+      _tone(note_A6, 55, 25);
+      _tone(note_E6, 50, 10);
+      break;
+    case 3: // S_buttonPushed
+      bendTones(note_E6, note_G6, 1.03, 20, 2);
+      delay(30);
+      bendTones(note_E6, note_D7, 1.04, 10, 2);
+      break;
+    case 4: // S_mode1
+      bendTones(note_E6, note_A6, 1.02, 30, 10);
+      break;
+    case 5: // S_mode2
+      bendTones(note_G6, note_D7, 1.03, 30, 10);
+      break;
+    case 6: // S_surprise
+      bendTones(800, 2150, 1.02, 10, 1);
+      bendTones(2149, 800, 1.03, 7, 1);
+      break;
+    case 7: // S_OhOoh
+      bendTones(880, 2000, 1.04, 8, 3);
+      delay(200);
+      for (float i = 880; i < 2000; i = i * 1.04) {
+        _tone(note_B5, 5, 10);
+      }
+      break;
+    case 8: // S_OhOoh2
+      bendTones(1880, 3000, 1.03, 8, 3);
+      delay(200);
+      for (float i = 1880; i < 3000; i = i * 1.03) {
+        _tone(note_C6, 10, 10);
+      }
+      break;
+    case 9: // S_cuddly
+      bendTones(700, 900, 1.03, 16, 4);
+      bendTones(899, 650, 1.01, 18, 7);
+      break;
+    case 10: // S_sleeping
+      bendTones(100, 500, 1.04, 10, 10);
+      delay(500);
+      bendTones(400, 100, 1.04, 10, 1);
+      break;
+    case 12: // S_happy
+      bendTones(1500, 2500, 1.05, 20, 8);
+      bendTones(2499, 1500, 1.05, 25, 8);
+      break;
+    case 13: // S_superHappy
+      bendTones(2000, 6000, 1.05, 8, 3);
+      delay(50);
+      bendTones(5999, 2000, 1.05, 13, 2);
+      break;
+    case 14: // S_sad
+      bendTones(880, 669, 1.02, 20, 200);
+      break;
+    case 15: // S_confused
+      bendTones(1000, 1700, 1.03, 8, 2);
+      bendTones(1699, 500, 1.04, 8, 3);
+      bendTones(1000, 1700, 1.05, 9, 10);
+      break;
+    case 17: // S_fart1
+      bendTones(1600, 3000, 1.02, 2, 15);
+      break;
+    case 18: // S_fart2
+      bendTones(2000, 6000, 1.02, 2, 20);
+      break;
+    case 19: // S_fart3
+      bendTones(1600, 4000, 1.02, 2, 20);
+      bendTones(4000, 3000, 1.02, 2, 20);
+      break;
+  }
+}
 
 #if not defined(ARDUINO_ARCH_ESP32)  // disable LineFollower ... Esp32 only has one analog ... maybe fix to use with digital line sensors
 int line_sensor_right = A0;
@@ -263,7 +371,7 @@ void setup() {
   bluetooth->begin("OttoDIY");
 
   Serial.begin(9600);
-  Ottobot.init(4, 5, 6, 7, true, BUZZER);
+  pinMode(BUZZER, OUTPUT);
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
 
@@ -276,6 +384,8 @@ void setup() {
   // Set the 50Hz frequency required by standard servos
   servo_right.setPeriodHertz(50);
   servo_left.setPeriodHertz(50);
+
+  gpio_set_drive_capability((gpio_num_t)BUZZER, GPIO_DRIVE_CAP_3);
 #endif
 
   attachServos();
@@ -355,7 +465,7 @@ void checkBluetooth() {
       p++;
     }
     int songName = atoi(p);
-    Ottobot.sing(songName);
+    sing(songName);
   }
 }
 
