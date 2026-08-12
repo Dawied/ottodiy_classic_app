6c9db92e209961ad76859d1699f9f8b82bdc324f
Dawied <Dawied@users.noreply.github.com>
2026-08-12 12:03:51 +0000

    Deploying to gh-pages from @ Dawied/ottodiy_classic_app@3c0b833f1b76e755e4ecbf6545d3946bf828ccad 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624aa1","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1404943401" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3855811898" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
